bf8ebfb3dbdb068814339ecd1f2301d4ca121ffc
dyedwiper <maximilian.becker@dataport.de>
2026-06-29 16:22:39 +0200

    Fix typing

--- ddd/src/shared/domain/aggregateRoot.ts
+++ ddd/src/shared/domain/aggregateRoot.ts
@@ -1,4 +1,4 @@
-import { Entity } from './entity.js';
+import { Entity, type EntityProps } from './entity.js';
 
 // See https://khalilstemmler.com/articles/typescript-domain-driven-design/aggregate-design-persistence/
-export abstract class AggregateRoot<T> extends Entity<T> {}
+export abstract class AggregateRoot<T extends EntityProps> extends Entity<T> {}
